2c3f64e388dc2a061fb13f95f8e3aef31308f177
Glenelly <glenellykarlatbm@gmail.com>
2023-09-11 12:33:53 -0300

    adicionar elemento no final do vetor

---
+++ src/vetores/Teste.java
@@ -0,0 +1,13 @@
+package vetores;
+
+public class Teste {
+    public static void main(String[] args) {
+        Vetor vetor = new Vetor(2);
+
+        vetor.adiciona("Elemento 1");
+        vetor.adiciona("Elemento 2");
+        vetor.adiciona("Elemento 3");
+
+
+    }
+}
