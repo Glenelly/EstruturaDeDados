a0b821b0191b722ce34d395f803b41aa2a38ebbe
Glenelly <glenellykarlatbm@gmail.com>
2023-09-11 20:27:31 -0300

    obter elemento de uma posição

---
+++ src/vetores/Teste3.java
@@ -0,0 +1,15 @@
+package vetores;
+
+public class Teste3 {
+    public static void main(String[] args) {
+
+        Vetor vetor = new Vetor(5);
+
+        vetor.adiciona("Elemento 1");
+        vetor.adiciona("Elemento 2");
+        vetor.adiciona("Elemento 3");
+
+        System.out.println(vetor.busca(1));
+
+    }
+}
